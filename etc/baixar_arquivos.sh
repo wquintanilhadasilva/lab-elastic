376b908d2079fb8bfaf761238e45b28d4e9013b1
Wedson Quintanilha da Silva <wedson.silva@oobj.com.br>
2023-11-03 18:11:11 -0300

    fix

--- etc/baixar_arquivos.sh
+++ etc/baixar_arquivos.sh
@@ -12,7 +12,7 @@ do
   curl "http://localhost:8080/baixador?qtd=10000&emi=${CNPJ}" \
     -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7" \
     -H "Accept-Language: pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7" \
-    -H "Authorization: Basic Z2VyYWRvci1vb2JqOmdlcmFkb3IubTRuNGczciE=" \
+    -H "Authorization: Basic xxxxxxxx" \
     -H "Connection: keep-alive" \
     -H "Referer: http://geradorweb.oobj.com.br/gerador-web/" \
     --compressed \
